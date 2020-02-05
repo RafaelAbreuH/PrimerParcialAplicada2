152742f8a20ec0ddd451228793fcedc0510113e4
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2020-02-05 18:11:25 -0400

    Add project files.

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS; Database= ExamenDb; Trusted_Connection = True;");
+        }
+    }
+}
